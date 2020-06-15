dbe541035614cad952398f4beb126311ed5ca8a2
Jiří Zapletal <zapletal@wakers.cz>
2020-06-15 19:58:14 +0200

    Added contact-form

---
+++ app/AppModule/Presenter/ContactPresenter.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) 2020 Wakers.cz
+ * @author Jiří Zapletal (https://www.wakers.cz, zapletal@wakers.cz)
+ */
+declare(strict_types=1);
+
+namespace App\AppModule\Presenter;
+
+use App\AppModule\Component\BreadCrumb\Entity\BreadCrumbItem;
+use App\BaseModule\Presenter\BasePresenter;
+use Doctrine\Common\Collections\ArrayCollection;
+
+final class ContactPresenter extends BasePresenter
+{
+    public function beforeRender()
+    {
+        parent::beforeRender();
+
+        $this->setLayout(__DIR__ . '/templates/@frontend.latte');
+
+        $breadCrumbItems = new ArrayCollection([
+            new BreadCrumbItem('Kontakty', 'Contact:summary')
+        ]);
+
+        $this->breadCrumbComponent->setBreadCrumbItems($breadCrumbItems);
+    }
+}
